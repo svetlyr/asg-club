756380785bd52e7ade2dbbfe7cc9a182d5f17135
svetly <radu.svetly@protonmail.com>
2025-02-02 19:50:50 +0200

    add navbar collapse on scroll functionality

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,57 +1,138 @@
-import { createSignal, onCleanup, onMount } from "solid-js";
+import { createSignal, For, onCleanup, onMount } from "solid-js";
 import type { Component } from "solid-js";
 
 import Link from "./link";
-import logo from "@assets/astro.svg";
 
-const Navbar: Component = () => {
+import logo from "@assets/logo.svg";
+import Motorcycle from "@icons/fa6-solid/motorcycle";
+
+const NAV_ITEMS = [
+    { name: "Services", href: "/" },
+    { name: "About Us", href: "/" },
+    { name: "Contact Us", href: "/" },
+];
+
+type Props = {
+    navCollapseId: string;
+};
+
+// eslint-disable-next-line solid/no-destructure
+const Navbar: Component<Props> = ({ navCollapseId }) => {
+    const navbarHeight = 84;
     const [isCollapsed, setIsCollapsed] = createSignal<boolean>(false);
 
-    let lastScrollY: number;
-    let ticking: boolean = false;
+    let lastScrollY = 0;
+    let ticking = false;
+    let targetOutOfView = false;
+    let observer: IntersectionObserver;
 
     const handleScroll = (): void => {
-        const currentScrollY = window.scrollY;
-        const shouldCollapse = currentScrollY > lastScrollY;
-
         if (ticking) return;
-
-        if (isCollapsed() === shouldCollapse) {
-            lastScrollY = currentScrollY;
-            return;
-        }
 
         ticking = true;
         window.requestAnimationFrame(() => {
-            setIsCollapsed(shouldCollapse);
+            const currentScrollY = window.scrollY;
+            const scrollUp = currentScrollY < lastScrollY;
+
+            if (scrollUp) {
+                setIsCollapsed(false);
+            } else if (targetOutOfView) {
+                setIsCollapsed(true);
+            }
+
             lastScrollY = currentScrollY;
             ticking = false;
         });
     };
 
+    const observeTarget = ([entry]: IntersectionObserverEntry[]): void => {
+        if (!entry) return;
+
+        targetOutOfView = !entry.isIntersecting;
+    };
+
     onMount(() => {
         lastScrollY = window.scrollY;
         window.addEventListener("scroll", handleScroll, { passive: true });
-        onCleanup(() => window.removeEventListener("scroll", handleScroll));
+
+        const target = document.getElementById(navCollapseId);
+        if (target) {
+            observer = new IntersectionObserver(observeTarget, {
+                root: null,
+                threshold: 1,
+                rootMargin: `-${navbarHeight}px 0px 0px 0px`,
+            });
+            observer.observe(target);
+        }
+
+        onCleanup(() => {
+            window.removeEventListener("scroll", handleScroll);
+            if (observer) observer.disconnect();
+        });
     });
 
     return (
         <header
-            class={`fixed top-0 flex w-full items-center bg-black-primary shadow-md transition-transform duration-300 ${isCollapsed() ? "-translate-y-full" : "translate-y-0"}`}
+            style={{ height: `${navbarHeight}px` }}
+            classList={{
+                "-translate-y-full": isCollapsed(),
+                "translate-y-0": !isCollapsed(),
+            }}
+            class="fixed top-0 w-full bg-black-primary shadow-md transition-transform duration-300"
         >
-            <Link href="/">
-                {/* TODO: logo */}
-                <img src={logo.src} alt="Logo" class="bg-white" />
-            </Link>
-            <ul>
-                <li class="text-white">
-                    <Link href="/">Services</Link>
-                    <Link href="/">About Us</Link>
-                    <Link href="/">Contact</Link>
-                </li>
-            </ul>
+            <div class="mx-auto flex h-full max-w-screen-xl items-center justify-between px-4 sm:px-6 md:px-8 lg:px-12">
+                <Link href="/" class="flex items-center">
+                    <img src={logo.src} alt="Logo" class="size-12 sm:size-14 md:size-16" />
+                    {/*  TODO: custom font this*/}
+                    <span class="ml-1 text-3xl font-bold md:text-4xl lg:text-5xl"> Club</span>
+                </Link>
+
+                <ul class="hidden items-center space-x-4 font-poppins text-white md:flex md:space-x-6 lg:space-x-8">
+                    <For each={NAV_ITEMS}>
+                        {({ name, href }) => (
+                            <li>
+                                <Link href={href} class="text-sm hover:text-gray-300 md:text-base lg:text-lg">
+                                    {name}
+                                </Link>
+                            </li>
+                        )}
+                    </For>
+                    <button class="flex items-center bg-[#FC5030] px-4 py-2 font-poppins font-semibold text-white md:px-6 md:py-2.5">
+                        <Motorcycle class="mr-2 size-4 md:size-5" /> <span class="text-sm md:text-base">ORDER</span>
+                    </button>
+                </ul>
+            </div>
         </header>
     );
+
+    // return (
+    //     <header
+    //         style={{ height: `${navbarHeight}px` }}
+    //         classList={{ "-translate-y-full": isCollapsed(), "translate-y-0": !isCollapsed() }}
+    //         class="fixed top-0 w-full bg-black-primary shadow-md transition-transform duration-300"
+    //     >
+    //         <div class="mx-auto flex h-full items-center justify-between sm:px-4">
+    //             <Link href="/" class="flex items-center">
+    //                 <img src={logo.src} alt="Logo" class="size-16" />
+    //                 {/* TODO: custom font this */}
+    //                 <span class="ml-1 text-5xl font-bold">Club</span>
+    //             </Link>
+    //             <ul class="flex items-center space-x-6 font-poppins text-white">
+    //                 <For each={NAV_ITEMS}>
+    //                     {({ name, href }) => (
+    //                         <li>
+    //                             <Link href={href}>{name}</Link>
+    //                         </li>
+    //                     )}
+    //                 </For>
+    //                 <button class="flex items-center bg-[#FC5030] px-6 py-2.5 font-poppins font-semibold text-white">
+    //                     <Motorcycle class="mr-2 size-5" />
+    //                     ORDER
+    //                 </button>
+    //             </ul>
+    //         </div>
+    //     </header>
+    // );
 };
 
 export default Navbar;
