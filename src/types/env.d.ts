7cf892545a42d6facabefdb3a714634914d3209f
svetly <radu.svetly@protonmail.com>
2025-05-09 19:51:39 +0300

    remove unused file

--- src/types/env.d.ts
+++
@@ -1,6 +0,0 @@
-import type { Env as ServerEnv } from "../env/client";
-import type { Env as ClientEnv } from "../env/server";
-
-export type Env = ServerEnv & ClientEnv;
-
-export declare const env: Env;
