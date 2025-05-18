d23632d7238e627bbb11285797de267c80cbb6ea
svetly <radu.svetly@protonmail.com>
2025-05-18 21:32:13 +0300

    feat: contact us

--- src/types/icons.d.ts
+++ src/types/icons.d.ts
@@ -12,5 +12,10 @@ declare module "@icons/*" {
 
 declare module "@icons" {
     import type { ComponentProps, JSX } from "solid-js";
-    export type IconType = (props: ComponentProps<"svg">) => JSX.Element;
+
+    export type IconType =
+        // * Solid
+        | ((props: ComponentProps<"svg">) => JSX.Element)
+        // * Astro
+        | (((_props: astroHTML.JSX.SVGAttributes) => unknown) & ImageMetadata);
 }
