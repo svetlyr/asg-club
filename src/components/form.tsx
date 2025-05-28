f8d73ce4818cfc6632916d9c1b8c7689a0eefadb
Plastelin Daniel <plastelin.daniel@protonmail.com>
2025-05-28 21:10:39 +0300

    fix: image path and validation

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -14,37 +14,41 @@ import ArrowRight from "@icons/sli/arrow-right";
 
 import useMultiStepForm from "@hooks/useMultiStepForm";
 
+import bike2 from "@assets/images/bike2.webp";
+import bike3 from "@assets/images/bike3.webp";
+import coffe2 from "@assets/images/coffee2.webp";
+
 const STEPS_DATA = [
     {
         title: "Order",
         paragraph:
             "Don't wait. Just fill the form below with your desires related to our services and we'll do our best to make your wishes come true. Please fill in all requested fields for best results.",
-        imageName: "coffee2",
+        image: coffe2.src,
     },
     {
         title: "Details",
         paragraph:
             "We need some important information to understand the basics of your order and have a better idea of what you want.",
-        imageName: "bike2",
+        image: bike2.src,
     },
     {
         title: "Almost Done",
         paragraph: "Comments",
-        imageName: "bike3",
+        image: bike3.src,
     },
 ];
 
 const MainForm: Component = () => {
     const formData = createFormStore<OrderSchema>({
         validateOn: "input",
-        validate: () => {
+        validate: (value) => {
             switch (currentStepIndex()) {
                 case 0:
-                    return valiForm(basicDetailsSchema);
+                    return valiForm(basicDetailsSchema)(value);
                 case 1:
-                    return valiForm(serviceDetailsSchema);
+                    return valiForm(serviceDetailsSchema)(value);
                 case 2:
-                    return valiForm(orderSchema);
+                    return valiForm(orderSchema)(value);
                 default:
                     return {};
             }
@@ -65,8 +69,8 @@ const MainForm: Component = () => {
     return (
         <div class="my-32 flex items-start justify-center gap-x-16">
             <Form of={formData} class="w-full lg:max-w-xl" onSubmit={handleSubmit}>
-                <h2 class="mb-6 text-center text-5xl lg:text-left">{STEPS_DATA[currentStepIndex()]?.title}</h2>
-                <p class="mb-20 text-center lg:text-left">{STEPS_DATA[currentStepIndex()]?.paragraph}</p>
+                <h2 class="mb-6 text-center text-5xl lg:text-left">{STEPS_DATA[currentStepIndex()].title}</h2>
+                <p class="mb-20 text-center lg:text-left">{STEPS_DATA[currentStepIndex()].paragraph}</p>
                 <Stepper currentStep={currentStepIndex} />
                 <div class="space-y-4">{step()}</div>
                 <div class="mt-4 flex gap-x-5">
@@ -88,7 +92,7 @@ const MainForm: Component = () => {
             <img
                 width={522}
                 class="hidden pr-4 lg:block"
-                src={`src/assets/images/${STEPS_DATA[currentStepIndex()]?.imageName}.webp`}
+                src={STEPS_DATA[currentStepIndex()].image}
                 loading="lazy"
                 decoding="async"
             />
